0456cf7d6a1ba0e595258df1391b6a4b3293e8a7
pk15pi <76875206+pk15pi@users.noreply.github.com>
2025-06-02 23:42:58 +0530

    non type article test condition reoved

--- tests/pid_minter_test/test_pid_minter.py
+++ tests/pid_minter_test/test_pid_minter.py
@@ -27,16 +27,6 @@ def test_pid_minter_with_pid(mock_connect, citation_object_with_pid):
     result = pid_minter(citation_object_with_pid)
     assert result == [citation_object_with_pid, "PID Already Assigned", None]
 
-@patch('pid_minter.mint_pid.connect_pid_db')
-def test_pid_minter_non_article(mock_connect, non_article_citation_object):
-    mock_connection = MagicMock()
-    mock_cursor = MagicMock()
-    mock_cursor.fetchone.return_value = [5]
-    mock_connection.cursor.return_value = mock_cursor
-    mock_connect.return_value = mock_connection
-
-    result = pid_minter(non_article_citation_object)
-    assert result == [non_article_citation_object, "Non Article", None]
 
 @patch('pid_minter.mint_pid.connect_pid_db')
 def test_pid_minter_db_connection_error(mock_connect, citation_object_no_pid):
