af784c39d874186e44328061404fc1fbf783c20d
pk15pi <76875206+pk15pi@users.noreply.github.com>
2025-06-03 22:24:09 +0530

    result message changed in mint pid

--- tests/pid_minter_test/test_pid_minter.py
+++ tests/pid_minter_test/test_pid_minter.py
@@ -13,7 +13,7 @@ def test_pid_minter_no_pid(mock_connect, citation_object_no_pid):
     mock_connect.return_value = mock_connection
 
     result = pid_minter(citation_object_no_pid)
-    assert result == [citation_object_no_pid, "PID Assigned", 5]
+    assert result == [citation_object_no_pid, "successful", "PID Assigned", 5]
     assert citation_object_no_pid.local.identifiers['pid'] == 5
 
 
@@ -22,5 +22,5 @@ def test_pid_minter_db_connection_error(mock_connect, citation_object_no_pid):
     mock_connect.return_value = None
 
     result = pid_minter(citation_object_no_pid)
-    assert result == [citation_object_no_pid,
+    assert result == [citation_object_no_pid, "unsuccessful",
                       "Database connection error occured", None]
